e6f5a8975e737b2c4cea517a5d8b5da70456627c
David Morris <davidrowlandmorris@gmail.com>
2015-03-19 16:20:36 +0000

    List repos with untracked files

--- forgetmenot.sh
+++ forgetmenot.sh
@@ -1,3 +1,27 @@
-#!/bin/bash
+#!/bin/bash -e
+
+checks='untracked'
+
+function indent {
+    sed -e 's/^/\t/'
+}
+
+function untracked {
+    git ls-files --others --exclude-standard | indent
+}
+
+function status {
+    if [[ "$checks" == *"untracked"* ]]; then
+        untracked
+    fi
+}
+
 find ~ -name '.git' -type d |
-    sed -e 's/\.git$//'
+    sed -e 's/\.git$//' |
+    while read x; do
+        cd "$x"
+        status=`status`
+        if [ -n "$status" ]; then
+            echo -e "$x\n$status"
+        fi
+    done
