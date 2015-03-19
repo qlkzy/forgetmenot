37947fa4c21c4c7dac07aefb7d9fd69a1dd3697d
David Morris <davidrowlandmorris@gmail.com>
2015-03-19 17:00:55 +0000

    Provide options for different checks

--- forgetmenot.sh
+++ forgetmenot.sh
@@ -1,14 +1,46 @@
 #!/bin/bash -e
 
-checks='^\(??\|A\| M| D\)'
+# checks='^\(??\|A\| M| D\)'
+
+checks=$1
+if [ -z "$checks" ]; then
+    checks='untracked,added,modified,deleted'
+fi
 
 function indent {
     sed -e 's/^/\t/'
 }
-p
+
 function status {
-    git status --porcelain --short | (grep "$checks" || true)
+    git status --porcelain --short | (grep "$regex" || true)
 }
+
+function addalt {
+    local re="$1"
+    if [ -n "$re" ]; then
+        re="$re\|"
+    fi
+    echo "$re$2"
+}
+
+regex=''
+
+case $checks in
+    *untracked*)
+        regex=`addalt "$regex" '??'`
+    ;;&
+    *added*)
+        regex=`addalt "$regex" 'A'`
+    ;;&
+    *modified*)
+        regex=`addalt "$regex" ' M'`
+    ;;&
+    *deleted*)
+        regex=`addalt "$regex" ' D'`
+    ;;&
+esac
+
+regex="^\\($regex\\)"
 
 find ~ -name '.git' -type d |
     sed -e 's/\.git$//' |
